c71471886c4d86d66224cae5c2d219ae4b14f2f4
FrenkenFlores <fflores@student.21-school.ru>
2020-12-30 12:34:47 +0300

    update #3: ssl, redirect, reverse

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Nginx_k8s_metallb
+https://wiki.alpinelinux.org/wiki/Nginx
+https://www.dmosk.ru/miniinstruktions.php?mini=nginx-redirects
--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-minikube --vm-driver=virtualbox start --extra-config=apiserver.service-node-port-range=1-35000
-minikube addons enable metallb
-minikube addons enable dashboard
-minikube addons enable metrics-server
-eval $(minikube docker-env)
-kubectl apply -f ./metallb-configmap.yaml
+#minikube --vm-driver=virtualbox start --extra-config=apiserver.service-node-port-range=1-35000
+#minikube addons enable metallb
+#minikube addons enable dashboard
+#minikube addons enable metrics-server
+#eval $(minikube docker-env)
 docker build -t nginx-image ./nginx/
+kubectl apply -f ./metallb-configmap.yaml
 kubectl apply -f ./nginx/nginx.yaml
