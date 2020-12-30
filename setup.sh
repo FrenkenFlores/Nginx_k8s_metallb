0ed137509a1054028957d4f10df9822623d261ff
FrenkenFlores <fflores@student.21-school.ru>
2020-12-30 14:25:07 +0300

    update #4

--- setup.sh
+++ setup.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 #minikube --vm-driver=virtualbox start --extra-config=apiserver.service-node-port-range=1-35000
-#minikube addons enable metallb
-#minikube addons enable dashboard
-#minikube addons enable metrics-server
-#eval $(minikube docker-env)
+minikube addons enable metallb
+minikube addons enable dashboard
+eval $(minikube docker-env)
 docker build -t nginx-image ./nginx/
 kubectl apply -f ./metallb-configmap.yaml
 kubectl apply -f ./nginx/nginx.yaml
